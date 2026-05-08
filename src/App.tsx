f00218b4db835df9eaa50bd75ca33fc26c5051a6
Venkat-023 <venkatbaba23@gmail.com>
2026-05-08 21:40:25 +0530

    Deploy Vite build to GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <div className="relative min-h-screen">
           <ParticleBackground />
           <Navigation />
